9cd78220021a9709963f1af499eed087587f06f3
Gusta-S <gustavo13fonseca@gmail.com>
2026-09-01 17:04:44 -0300

    Iniciando busca em Eldorado

---
+++ AplicaThon/EldoradoFind.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+
+driver = webdriver.Firefox()
+driver.get("https://institutoeldorado.gupy.io/")
