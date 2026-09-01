fdc5ec2139c96ad729ea4538e8bf82c29062b7f0
Gusta-S <gustavo13fonseca@gmail.com>
2026-09-01 19:49:27 -0300

    Arrumando bug de input XPath para Css_Selector

--- AplicaThon/EldoradoFind.py
+++ AplicaThon/EldoradoFind.py
@@ -11,24 +11,29 @@ assert "Vagas Instituto de Pesquisas Eldorado" in driver.title
 estado = driver.find_element(By.XPATH, "/html/body/div[1]/div/main/section[3]/div[2]/form[2]/div[4]/div/div/div[1]/div[2]")
 driver.execute_script("arguments[0].scrollIntoView({block: 'center'});",estado)
 estado.click()
-submenu_estado = driver.find_element(By.XPATH, "/html/body/div[1]/div/main/section[3]/div[2]/form[2]/div[4]/div")
-submenu_estado.send_keys("São Paulo (SP)")
+submenu_estado = driver.find_element(By.CSS_SELECTOR, "#state-select")
+driver.execute_script("arguments[0].scrollIntoView({block: 'center'});",submenu_estado)
+submenu_estado.click()
+submenu_estado.send_keys("São Paulo")
 submenu_estado.send_keys(Keys.TAB)
 
 #Filtre por Cidade
-cidade = driver.find_element(By.XPATH, "/html/body/div[1]/div/main/section[3]/div[2]/form[2]/div[5]/div")
+cidade = driver.find_element(By.CSS_SELECTOR, "#city-select")
+driver.execute_script("arguments[0].scrollIntoView({block: 'center'});",cidade)
 cidade.click()
 cidade.send_keys("Campinas")
 cidade.send_keys(Keys.TAB)
 
 #Filtre o tipo de vaga 
-tipo_vaga = driver.find_element(By.XPATH, "/html/body/div[1]/div/main/section[3]/div[2]/form[2]/div[2]/div/div/div[1]/div[2]")
+tipo_vaga = driver.find_element(By.CSS_SELECTOR, "#job-type-select")
+driver.execute_script("arguments[0].scrollIntoView({block: 'center'});",tipo_vaga)
 tipo_vaga.click()
 tipo_vaga.send_keys("Estágio")
 tipo_vaga.send_keys(Keys.TAB)
 
 #Selecione área da vaga 
-area = driver.find_element(By.XPATH, "/html/body/div[1]/div/main/section[3]/div[2]/form[2]/div[3]/div/div/div[1]/div[2]")
+area = driver.find_element(By.CSS_SELECTOR, "#department-select")
+driver.execute_script("arguments[0].scrollIntoView({block: 'center'});",area)
 area.click()
 area.send_keys("Software")
 area.send_keys(Keys.TAB)
